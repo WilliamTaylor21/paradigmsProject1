60dd7f76e07147806962cdae79b964d43080a69d
WilliamTaylor21 <71159114+WilliamTaylor21@users.noreply.github.com>
2020-09-11 19:54:38 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,10 +1,3 @@
-//William Taylor
-//9/11/2020
-//Programming Paradigms Assignment1 @ University of Arkansas:
-//Program to make a turtle track a mouse click.
-//Turtle can also be moved with arrow keys.
-
-
 ::@echo off
 javac Game.java View.java Controller.java Model.java
 if %errorlevel% neq 0 (
